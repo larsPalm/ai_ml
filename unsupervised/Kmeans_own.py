88d171dd0c0f30a53d08f34b6fbc107fafa8dedc
larspalm <lcpalm@ifi.uio.no>
2020-05-06 21:12:53 +0200

    litt mer optimalisert, samt plotting for ulike verdier av k

--- unsupervised/Kmeans_own.py
+++ unsupervised/Kmeans_own.py
@@ -7,6 +7,7 @@ from sklearn.cluster import KMeans
 from pca import pca
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
+from numba import jit
 
 
 class Kmeans_own:
@@ -18,16 +19,20 @@ class Kmeans_own:
         self.changed = True
         self.teller = 0
 
-    def fit(self, X, epochs=100):
-        start = 0
+    def fit(self, X, epochs=50):
+        """start = 0
         stop = int(X.shape[0]/self.k)
         for i in range(self.k):
             if i+1 == self.k:
                 stop = X.shape[0]
-            print(start, stop)
             self.clusters.append(X[start:stop,:])
             start = stop
-            stop = stop + int(X.shape[0]/self.k)
+            stop = stop + int(X.shape[0]/self.k)"""
+        self.clusters = [[] for _ in range(self.k)]
+        for i in range(X.shape[0]):
+            index = random.randint(0, self.k - 1)
+            self.clusters[index].append(X[i])
+            self.sample_in_cluster.append(index)
         for e in range(epochs):
             #beregn nye centers
             self.estimate_centers()
@@ -35,6 +40,8 @@ class Kmeans_own:
             self.reset_clusters()
             #legg til alle punkter på nytt i clusters
             self.make_clusters(X)
+            if self.changed == False:
+                break
 
     def fit2(self, X):
         self.clusters = [[] for _ in range(self.k)]
@@ -43,31 +50,31 @@ class Kmeans_own:
             self.clusters[index].append(X[i])
             self.sample_in_cluster.append(index)
         teller = 0
-        while self.changed and teller < 200:
+        while self.changed and teller < 50:
             #beregn nye centers
             self.estimate_centers()
             #nullstill clusters
             self.reset_clusters()
             #legg til alle punkter på nytt i clusters
-            self.make_clusters2(X)
+            self.make_clusters(X)
             teller += 1
-        print(teller)
+        #print(teller)
+
+    """def make_clusters(self,X):
+        for i in range(X.shape[0]):
+            self.find_cluster(X[i])"""
 
     def make_clusters(self,X):
-        for i in range(X.shape[0]):
-            self.find_cluster(X[i])
-
-    def make_clusters2(self,X):
         self.changed = False
         for i in range(X.shape[0]):
-            self.find_cluster2(X[i],i)
+            self.find_cluster(X[i],i)
 
-    def find_cluster(self,x):
+    """def find_cluster(self,x):
         dist = [self.distance_L2(x, np.asarray(self.centers[ks])) for ks in range(self.k)]
         cluster_index = dist.index(min(dist))
-        self.clusters[cluster_index].append(x)
+        self.clusters[cluster_index].append(x)"""
 
-    def find_cluster2(self, x,i):
+    def find_cluster(self, x,i):
         dist = [self.distance_L2(x, np.asarray(self.centers[ks])) for ks in range(self.k)]
         cluster_index = dist.index(min(dist))
         if cluster_index != self.sample_in_cluster[i]:
@@ -83,8 +90,6 @@ class Kmeans_own:
     def estimate_centers(self):
         for x in range(self.k):
             cluster = np.asarray(self.clusters[x])
-            if cluster.shape[0]==0:
-                print('???',self.teller)
             if cluster.shape[0]>0:
                 self.centers[x] = [np.mean(cluster[:, i]) for i in range(cluster.shape[1])]
         self.teller += 1
@@ -112,6 +117,9 @@ if __name__ == '__main__':
     km = Kmeans_own(3)
     km.fit2(P)
     pred = km.predict(P)
+    km2 = Kmeans_own(3)
+    km2.fit2(P)
+    pred2 = km2.predict(P)
     print(P.shape)
     plt.figure()
     plt.scatter(P[:, 0], P[:, 1], c=pred)
@@ -119,4 +127,12 @@ if __name__ == '__main__':
     plt.figure()
     plt.scatter(P[:, 0], P[:, 1], c=sk_pred)
     plt.title('sklearn with k = {}'.format(3))
+    plt.show()
+    for k in range(3,10,2):
+        km = Kmeans_own(k)
+        km.fit2(P)
+        pred = km.predict(P)
+        plt.figure()
+        plt.scatter(P[:, 0], P[:, 1], c=pred)
+        plt.title('own with k= {}'.format(k))
     plt.show()
