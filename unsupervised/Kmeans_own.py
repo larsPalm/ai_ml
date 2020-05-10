38281ee997844b4aa5c51382cde41ce90029b2f0
larspalm <lcpalm@ifi.uio.no>
2020-05-10 17:17:55 +0200

    flyttet testingen av koden til fil: test_kmeans_own.py

--- unsupervised/Kmeans_own.py
+++ unsupervised/Kmeans_own.py
@@ -104,35 +104,3 @@ class Kmeans_own:
 
     def predict(self, X):
         return [self.estimate_cluster(X[i]) for i in range(X.shape[0])]
-
-
-
-
-if __name__ == '__main__':
-    X, y = syntheticdata.get_iris_data()
-    _, P = pca(X, 2)
-    sk_km = KMeans(3)
-    sk_km.fit(P)
-    sk_pred = sk_km.predict(P)
-    km = Kmeans_own(3)
-    km.fit2(P)
-    pred = km.predict(P)
-    km2 = Kmeans_own(3)
-    km2.fit2(P)
-    pred2 = km2.predict(P)
-    print(P.shape)
-    plt.figure()
-    plt.scatter(P[:, 0], P[:, 1], c=pred)
-    plt.title('own with k= {}'.format(3))
-    plt.figure()
-    plt.scatter(P[:, 0], P[:, 1], c=sk_pred)
-    plt.title('sklearn with k = {}'.format(3))
-    plt.show()
-    for k in range(3,10,2):
-        km = Kmeans_own(k)
-        km.fit2(P)
-        pred = km.predict(P)
-        plt.figure()
-        plt.scatter(P[:, 0], P[:, 1], c=pred)
-        plt.title('own with k= {}'.format(k))
-    plt.show()
